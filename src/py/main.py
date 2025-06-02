2f7e7296adc5b11dda866d2ba31adcc83f56c433
unraous516 <unraous@qq.com>
2025-06-02 23:08:53 +0800

    bug修复

--- src/py/main.py
+++ src/py/main.py
@@ -101,23 +101,6 @@ if driver is None:
 driver.get("https://mooc1.chaoxing.com")  # 先访问主域，才能设置 Cookie
 
 
-cookie_str = """fid=193; _uid=341921830; _d=1748263678039; UID=341921830; vc3=BKViqMrSDlYGwHI25nMcL8yT3%2FLs0t7MRFGqYQEbr5gHilkYnS9wNN%2FJph3q4hbvbG8x1j5FAlsb54U58S7e%2FCWRZrcHvi0o9RaHhPIByUju8PnJFkpFXMg3wqJ60m4U7NSrKzgmyhx4p2aI8dFM7nSE3JNkpg2KOs5QBrsbfds%3Da51fdd5ceb676206cf142193e2cda472; uf=b2d2c93beefa90dcc5cca0e422da616fc1d6515ee9c2203a77cbfbb52e70a880b0fd89aa7ae44d2d69225eea218755dbc49d67c0c30ca5043ad701c8b4cc548c0234d89f51c3dccfbeb9b25df8545daf713028f1ec42bf71b1188854805578ccce71fc6e59483dd31147cb7c4f7a0d3c0e6626e58d6891607e630b53a9d42d2ce9fdc681bdf07734; cx_p_token=214d25aa4a798bc4cc43ce0df6c9485a; p_auth_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIzNDE5MjE4MzAiLCJsb2dpblRpbWUiOjE3NDgyNjM2NzgwNDAsImV4cCI6MTc0ODg2ODQ3OH0.18CdO-_gU53O0Qid_iq30nwJoF4FKiUCJ4uvMcW_SAQ; xxtenc=4807beab172611266f5ce283f927bd95; DSSTASH_LOG=C_38-UN_120-US_341921830-T_1748263678041; k8s=1748783509.144.9248.907606; jrose=4BC684B6BECCF8F750A103C733BD3B15.mooc-536300739-f1g2v; route=f537d772be8122bff9ae56a564b98ff6"""
-
-for item in cookie_str.split(";"):
-    name, value = item.strip().split("=", 1)
-    cookie_dict = {
-        "name": name,
-        "value": value,
-        "domain": ".chaoxing.com"
-    }
-    try:
-        driver.add_cookie(cookie_dict)
-    except Exception as e:
-        print(f"添加 cookie {name} 失败: {e}")
-
-driver.get("https://mooc1.chaoxing.com/mycourse/studentstudy?chapterId=869952226&courseId=248046494&clazzid=118872772&cpi=405841762&enc=d957912715df6fc2f6d7b24db3f90882&mooc2=1&openc=2892e6b9b3356785dc0fd19d71941f9d")
-
-
 
 
 input("欢迎使用此脚本，请登录后打开至目标课程界面（即达到旧版用来粘贴脚本的页面即可），准备完成后按回车以继续...\n")
